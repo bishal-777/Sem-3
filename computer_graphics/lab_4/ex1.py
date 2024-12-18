c24bcb3092f3ccfcc717a44c6770ad8f6b118f58
bishal-777 <bishal777giri@gmail.com>
2024-12-18 14:13:28 +0545

    added lab 4

---
+++ computer_graphics/lab_4/ex1.py
@@ -0,0 +1,61 @@
+#This program draws a circle using midpoint circle algorithm
+import pygame
+import sys
+
+# Initialize Pygame
+pygame.init()
+
+# Set up the display
+WIDTH, HEIGHT = 1200, 1080
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Circle")
+
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+# Function 
+def draw_circle(xc,yc,r):
+    x,y=0,r
+
+    p=1-r
+
+    while(x<=y):
+
+        screen.set_at((x+xc,y+yc),WHITE)
+        screen.set_at((y+xc,x+yc),WHITE)
+        screen.set_at((x+xc,-y+yc),WHITE)
+        screen.set_at((y+xc,-x+yc),WHITE)
+        screen.set_at((-y+xc,x+yc),WHITE)
+        screen.set_at((-x+xc,-y+yc),WHITE)
+        screen.set_at((-y+xc,-x+yc),WHITE)
+        screen.set_at((-x+xc,y+yc),WHITE)
+        x+=1
+
+        if p<0:
+            p+=2*x+1
+        else:
+            y-=1
+            p+=2*x-2*y+1
+
+
+
+
+# Main loop
+def main():
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        # Clear the screen
+        screen.fill(BLACK)
+        #function call
+        draw_circle(600,560,200)
+
+        # Update the display
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
---
+++ computer_graphics/lab_4/readme.md
@@ -0,0 +1,4 @@
+# Lab-3
+In this lab of computer graphics and visualization,I learned following things:
+* Apply Midpoint circle algorithm in code
+* Apply symmetric property of circle.
